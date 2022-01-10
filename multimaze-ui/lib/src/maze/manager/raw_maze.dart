748d81aa1c9dd9d716175693408bced525e432f7
Craig Labenz <craig.labenz@gmail.com>
2022-01-10 13:42:39 -0800

    made most maze corridors 2 squares wide

--- multimaze-ui/lib/src/maze/manager/raw_maze.dart
+++ multimaze-ui/lib/src/maze/manager/raw_maze.dart
@@ -1,7 +1,7 @@
 /// String representation of a Maze.
 ///
 /// Each string in the list is a row of squares in the maze. Each character in
-/// each string represent's that row's tile for the given column.
+/// each string represents that row's tile for the given column.
 ///
 /// All strings must be of equal length, as will be validated by `MazeManager`.
 ///
@@ -9,15 +9,17 @@
 ///   * space - empty square
 ///   * astericks - an impassible wall
 ///   * @ - the player's starting location
-///   * x - the target location / victory square
+///   * x - the target location / victory square (X marks the spot :))
 final List<String> rawMaze = [
-  "**********x*********",
-  "******     *        ",
-  "*      *****    ** *",
-  "* ***  *   ******* *",
-  "* * **** * *       *",
-  "* *      * **** ****",
-  "* ****** * **** ****",
-  "*        *         @",
-  "********************",
+  "**********x***********",
+  "******     *          ",
+  "*          *     **   ",
+  "*       ***********  *",
+  "*  **** *    *    *  *",
+  "*  *  ***    *       *",
+  "*  *      *  ***  ****",
+  "*  *****  *  ***  ****",
+  "*         *           ",
+  "*         *          @",
+  "**********************",
 ];
