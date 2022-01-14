afe6bfb03af50b47ca9cab092d75ffb3f6bd6c70
Frank van Puffelen <puf@google.com>
2022-01-14 14:12:16 -0800

    Now write new position to the database
    Writes the new position to the database, skipping all local processing (including error checks).
    
    Also added some disabled code to print the maze in a format that we can easily copy/paste into the Realtime Database.

--- multimaze-ui/lib/src/maze/manager/maze_manager.dart
+++ multimaze-ui/lib/src/maze/manager/maze_manager.dart
@@ -3,9 +3,10 @@ import 'package:multimaze/src/maze/maze.dart';
 import 'package:riverpod/riverpod.dart';
 
 class MazeManager extends StateNotifier<MazeData> {
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
   MazeManager() : super(parseMaze(rawMaze)) {
     // Dart implementation of https://firebase.google.com/docs/database/android/offline-capabilities#section-sample
-    FirebaseDatabase database = FirebaseDatabase.instance;
     database.ref('.info/connected').onValue.listen((event) {
       if (event.snapshot.value == true) {
         var connectionRef = database.ref('connections').push();
@@ -19,10 +20,7 @@ class MazeManager extends StateNotifier<MazeData> {
     });
     // listen to position from the database
     database.ref('position').onValue.listen((event) {
-      print('Got position snapshot: ${event.snapshot}');
       var data = event.snapshot.value as Map<String, dynamic>;
-      print('Got new position: $data');
-      // TODO: local client should also validate state?
       state = state.copyWith(
           playerLocation: Coordinates(x: data['x'], y: data['y']));
     });
@@ -48,23 +46,30 @@ class MazeManager extends StateNotifier<MazeData> {
     late Coordinates targetLocation;
 
     // Iterate over the string representation of a maze.
+    Map<String,bool> json = {};
     int yAxis = 0;
     for (String row in rawMaze.reversed) {
       int xAxis = 0;
       for (String square in row.split('')) {
         if (square == MazeData.rawWallLocation) {
           wallLocations.add(Coordinates(x: xAxis, y: yAxis));
+          json['$xAxis $yAxis'] = true;
         } else if (square == MazeData.rawPlayerLocation) {
           playerLocation = Coordinates(x: xAxis, y: yAxis);
         } else if (square == MazeData.rawTargetLocation) {
           targetLocation = Coordinates(x: xAxis, y: yAxis);
+          json['$xAxis $yAxis'] = false;
         } else if (square != MazeData.rawEmptySquare) {
           throw Exception('Encountered unexpected character in maze: $square');
         }
         xAxis += 1;
       }
-
       yAxis += 1;
+    }
+    // alternative JavaScript to split maze: ["+rawMaze.map((line, y) => "["+line.split("").map((c,x) => `"${c}"`).join(", ")+"], ").join("")+"]"
+    if (false) {
+      // This code prints the maze in a format that can be copy/pasted into the Realtime Database
+      print('MAZE JSON: { ${json.keys.map((key) => '"$key": ${json[key]}' ).join(", ")} }');
     }
 
     return MazeData(
@@ -81,16 +86,18 @@ class MazeManager extends StateNotifier<MazeData> {
   void send(MoveCommand command) {
     final oldX = state.playerLocation.x;
     final oldY = state.playerLocation.y;
-    // TODO: send new state to RTDB
-    // database.ref('location').set({
-    //   'x': ServerValue.increment(1),
-    // });
     final newLocation = command.map(
       up: (_) => Coordinates(x: oldX, y: oldY + 1),
       down: (_) => Coordinates(x: oldX, y: oldY - 1),
       left: (_) => Coordinates(x: oldX - 1, y: oldY),
       right: (_) => Coordinates(x: oldX + 1, y: oldY),
     );
+    // send new state to RTDB
+    database.ref('position').set({
+      'x': newLocation.x, 
+      'y': newLocation.y
+    });
+    return;
     if (newLocation.x < 0 ||
         newLocation.y < 0 ||
         newLocation.x > state.columns - 1 ||
